812316c217a6d68f2d9211590edd50bf578292f2
조인식 <imsif42@naver.com>
2024-05-23 13:33:17 +0900

    chore: shadcn/ui

---
+++ src/app/(beforeLogin)/_components/AuthButton.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default function AuthButton() {
+  return <div className="w-80 h-12 rounded-4xl bg-blue">로그인</div>;
+}
--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,3 +1,10 @@
+import AuthButton from '../_components/AuthButton';
+
 export default function Page() {
-  return <div>login page!</div>;
+  return (
+    <div>
+      login page!
+      <AuthButton />
+    </div>
+  );
 }
