dafca10ad92707f3347b167370676ca4e40de3b9
조인식 <imsif42@naver.com>
2024-05-30 00:44:07 +0900

    chore: swiper

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,6 +1,6 @@
 export default function Page() {
   return (
-    <div className="py-16 px-6 h-lvh bg-main">
+    <div className="py-16 px-6 h-max bg-main">
       <h1 className="font-galmuri text-3xl pb-3 text-white">GYMMI</h1>
       <h5 className="text-xs mb-6 text-white">
         지미와 함께 운동의욕을 채워보세요!
