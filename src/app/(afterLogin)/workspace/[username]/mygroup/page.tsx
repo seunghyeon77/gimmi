75eb134b570e44b908cb2440ff53f69db0b08db9
조인식 <imsif42@naver.com>
2024-06-11 00:06:42 +0900

    feat: useQuery-마이 리스트

--- src/app/(afterLogin)/workspace/[username]/mygroup/page.tsx
+++ src/app/(afterLogin)/workspace/[username]/mygroup/page.tsx
@@ -1,10 +1,5 @@
-'use client';
-
-import { Progress } from '@/components/ui/progress';
-import { generateWorkspaces } from '@/utils/fakerData';
 import Link from 'next/link';
-
-const workSpaceData = generateWorkspaces(4);
+import MyWorkspacelits from '../../_components/MyWorkspacelists';
 
 export default function Page() {
   return (
@@ -16,27 +11,7 @@ export default function Page() {
         </Link>
       </div>
       <div>
-        {workSpaceData.map((item) => {
-          return (
-            <div
-              className="bg-[#60A5FA] w-full h-[136px] rounded-lg p-4 mb-7"
-              key={item.id}
-            >
-              <h2 className="text-2xl mb-3.5 text-white">{item.title}</h2>
-              <div className="text-[10px] text-white">
-                {item.tags.map((tag, i) => (
-                  <span key={i}>{tag}</span>
-                ))}
-              </div>
-              <div className="text-[10px] text-white mb-3.5">
-                <span>{`테스크 점수: ${item.taskScore}점`}</span>
-                <span>, </span>
-                <span>{item.name}</span>
-              </div>
-              <Progress className="h-1.5" value={item.achievementRate} />
-            </div>
-          );
-        })}
+        <MyWorkspacelits />
       </div>
     </div>
   );
