aa3f52f48e277f847d8956ce9477d50cd80ee58e
조인식 <imsif42@naver.com>
2024-06-15 21:27:37 +0900

    feat: 생성 1,2페이지 에러처리 추가

--- src/app/(afterLogin)/create-workspace/first/page.tsx
+++ src/app/(afterLogin)/create-workspace/first/page.tsx
@@ -6,9 +6,10 @@ import { Label } from '@/components/ui/label';
 import { Progress } from '@/components/ui/progress';
 import Link from 'next/link';
 import { useWorkSpaceStore } from '@/hooks/useWorkSpaceStore';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { verlifyDuplication } from '@/api/duplication';
 import { duplicationType } from '@/constants/duplication';
+import { AxiosError } from 'axios';
 
 export default function Page() {
   const { groupMaker, add1Page } = useWorkSpaceStore();
@@ -16,13 +17,22 @@ export default function Page() {
   const [name, setName] = useState(groupMaker.name);
   const [headCount, setHeadCount] = useState(groupMaker.headCount);
 
+  const [error, setError] = useState('');
+  const [nameCheck, setNameCheck] = useState(false);
+
   const handleNext = () => {
     add1Page({ name, headCount });
   };
   const [disabled, setDisabled] = useState(true);
-  if (name.length > 1 && headCount > 1) {
-    setDisabled(false);
-  }
+
+  useEffect(() => {
+    // api 검사 마치면 && nameCheck 넣어주기
+    if (name.length > 1 && headCount > 1) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [name, headCount]);
 
   const duplicateGroupName = async (type: string) => {
     try {
@@ -30,8 +40,13 @@ export default function Page() {
         type,
         value: name,
       });
+      setError('');
+      setNameCheck(true);
       console.log(res);
     } catch (error) {
+      if (error instanceof AxiosError) {
+        setError('error');
+      }
       console.error(error);
     }
   };
@@ -41,15 +56,19 @@ export default function Page() {
     <>
       <Progress value={33} className="h-[1px] mb-9" />
       <div className="grid w-full max-w-sm items-center mb-11">
-        <Label htmlFor="id" className="text-xs text-[#6B7280] font-normal mb-2">
+        <Label
+          htmlFor="name"
+          className="text-xs text-[#6B7280] font-normal mb-2"
+        >
           1.그룹 이름을 정해주세요!
         </Label>
         <div className="flex justify-between items-center">
           <Input
+            autoFocus
             required
             maxLength={9}
             type="text"
-            id="id"
+            id="name"
             placeholder="최대 9자"
             className="w-56 h-[52px] bg-[#F9FAFB] placeholder:text-xs placeholder:text-[#D1D5DB]"
             value={name}
@@ -62,16 +81,22 @@ export default function Page() {
             중복확인
           </Button>
         </div>
+        {error !== '' ? <span className="text-red-500">{error}</span> : null}
       </div>
+
       <div className="grid w-full max-w-sm items-center">
-        <Label htmlFor="id" className="text-xs text-[#6B7280] font-normal mb-2">
+        <Label
+          htmlFor="groupNum"
+          className="text-xs text-[#6B7280] font-normal mb-2"
+        >
           2. 그룹 인원수를 설정하세요!
         </Label>
         <div className="flex justify-between items-center">
           <Input
+            autoFocus
             required
             type="number"
-            id="id"
+            id="groupNum"
             placeholder="최소 2명 ~ 최대 9명"
             className="w-56 h-[52px] bg-[#F9FAFB] placeholder:text-xs placeholder:text-[#D1D5DB]"
             value={headCount}
