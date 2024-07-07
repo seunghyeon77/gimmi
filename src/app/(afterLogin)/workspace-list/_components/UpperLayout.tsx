c9b6bc0c159bad87ba3fe5501b7a1244e4164e83
조인식 <imsif42@naver.com>
2024-07-07 19:23:06 +0900

    fix: 마이 워크스페이스 리스트 ui수정

--- src/app/(afterLogin)/workspace-list/_components/UpperLayout.tsx
+++ src/app/(afterLogin)/workspace-list/_components/UpperLayout.tsx
@@ -12,7 +12,7 @@ export default function UpperLayout() {
       <Image
         src={backArrow}
         alt="backArrow"
-        className="w-6 mb-1.5"
+        className="w-6 mb-3"
         onClick={() => router.back()}
       />
       <h1 className="font-galmuri text-3xl text-white pl-1 pb-1">MY</h1>
