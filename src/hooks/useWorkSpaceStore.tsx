7b705d9f8cc66e6c7fc81039c9afb01a5dcd7f0d
조인식 <imsif42@naver.com>
2024-07-13 18:31:52 +0900

    feat: zustand clear함수 생성

--- src/hooks/useWorkSpaceStore.tsx
+++ src/hooks/useWorkSpaceStore.tsx
@@ -1,23 +1,25 @@
 import { IW, IWorkspaceInputs } from '@/types/\bworkSpace';
 import { create } from 'zustand';
 
+const initialValue = {
+  name: '',
+  headCount: '',
+  goalScore: '',
+  description: '',
+  tag: '',
+  missionBoard: [
+    { id: 0, mission: '', placeholder: 'ex 풀업 10회', score: 0 },
+    { id: 1, mission: '', placeholder: 'ex 벤치프레스 10회', score: 0 },
+    { id: 2, mission: '', placeholder: '스쿼트 10회', score: 0 },
+    { id: 3, mission: '', placeholder: '러닝 20분', score: 0 },
+    { id: 4, mission: '', placeholder: '런지 10회', score: 0 },
+  ],
+  task: '',
+  checked: false,
+};
+
 export const useWorkSpaceStore = create<IW>((set) => ({
-  groupMaker: {
-    name: '',
-    headCount: '',
-    goalScore: '',
-    description: '',
-    tag: '',
-    missionBoard: [
-      { id: 0, mission: '', placeholder: 'ex 풀업 10회', score: 0 },
-      { id: 1, mission: '', placeholder: 'ex 벤치프레스 10회', score: 0 },
-      { id: 2, mission: '', placeholder: '스쿼트 10회', score: 0 },
-      { id: 3, mission: '', placeholder: '러닝 20분', score: 0 },
-      { id: 4, mission: '', placeholder: '런지 10회', score: 0 },
-    ],
-    task: '',
-    checked: false,
-  },
+  groupMaker: initialValue,
   add1Page: ({ name, headCount, checked }: IWorkspaceInputs) =>
     set((state) => ({
       groupMaker: { ...state.groupMaker, name, headCount, checked },
@@ -39,4 +41,5 @@ export const useWorkSpaceStore = create<IW>((set) => ({
         tag,
       },
     })),
+  clearData: () => set({ groupMaker: initialValue }),
 }));
