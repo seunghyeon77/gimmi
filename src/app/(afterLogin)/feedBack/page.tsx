96c09bb4f69b39c5c5aeaf5c0b7cea8929d2c27c
조인식 <imsif42@naver.com>
2024-07-25 14:37:45 +0900

    feat: 피드백 모달 추가

--- src/app/(afterLogin)/feedBack/page.tsx
+++ src/app/(afterLogin)/feedBack/page.tsx
@@ -4,16 +4,20 @@ import { feedBack } from '@/api/check';
 import BackArrow from '../_components/BackArrow';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { Dialog, DialogClose, DialogContent } from '@/components/ui/dialog';
 
 export default function Page() {
   const router = useRouter();
   const [value, setValue] = useState('');
+
+  const [open, setOpen] = useState(false);
   const handleSubmit = async () => {
+    if (value === '') return;
     try {
       const res = await feedBack(value);
       console.log(res);
       if (res.status === 200) {
-        router.back();
+        setOpen(true);
       }
     } catch (error) {
       console.error(error);
@@ -21,6 +25,22 @@ export default function Page() {
   };
   return (
     <div className="px-6 py-12 h-screen">
+      <Dialog open={open}>
+        <DialogContent className="w-9/12 rounded-lg h-36 ">
+          <div className="text-center text-base mt-4 mb-4">
+            정말정말 감사합니다
+          </div>
+
+          <div className="flex justify-around items-center border-t-[1px] -mx-6">
+            <div
+              className="text-[#3B82F6] py-3 px-12 text-sm"
+              onClick={() => router.back()}
+            >
+              간식받기💙
+            </div>
+          </div>
+        </DialogContent>
+      </Dialog>
       <BackArrow />
       <div className="h-full flex flex-col justify-between">
         <div>
