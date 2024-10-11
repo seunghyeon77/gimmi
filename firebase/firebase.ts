ad7458214db4af2af3b88aa0d2a5d6bc31815338
조인식 <imsif42@naver.com>
2024-10-11 19:02:24 +0900

    chore: firebase

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
+import { getMessaging, getToken } from 'firebase/messaging';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
@@ -19,3 +17,25 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+/**
+ * FCM 토큰 발급
+ */
+export const setTokenHandler = async () => {
+  const messaging = getMessaging(app);
+  await getToken(messaging, {
+    vapidKey: process.env.NEXT_PUBLIC_VAPID_KEY,
+  })
+    .then(async (currentToken) => {
+      if (!currentToken) {
+        // 토큰 생성 불가시 처리할 내용, 주로 브라우저 푸시 허용이 안된 경우에 해당한다.
+        console.error('토큰 생성 불가');
+      } else {
+        // 토큰을 받았다면 여기서 DB에 저장하면 됩니다.
+        console.log('currentToken', currentToken);
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
