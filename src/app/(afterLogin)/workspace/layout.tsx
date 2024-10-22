046f37996fd4dd8c887b7a705d210a9559380b8b
seunghyeon77 <jdyjsh77@naver.com>
2024-10-22 15:57:38 +0900

    style: 워크스페이스 네브바 디자인 추가

--- src/app/(afterLogin)/workspace/layout.tsx
+++ src/app/(afterLogin)/workspace/layout.tsx
@@ -1,5 +1,6 @@
-import { ReactNode } from 'react';
-import BackArrow from '../_components/BackArrow';
+import { ReactNode } from "react";
+
+import BackArrow from "../_components/BackArrow";
 
 type Props = {
   children: ReactNode;
@@ -9,6 +10,16 @@ export default function Layout({ children }: Props) {
   return (
     <div className="px-6 py-12 bg-custom-gradient2 h-full">
       <BackArrow />
+      <nav className="my-5">
+        <hr className="border w-screen -mx-6" />
+        <ul className="flex text-base gap-x-11 sm:gap-x-8 lg:gap-x-12 justify-center my-2.5">
+          <li>그룹홈</li>
+          <li>운동하기</li>
+          <li>그룹채팅</li>
+          <li>운동인증</li>
+        </ul>
+        <hr className="border w-screen -mx-6" />
+      </nav>
       {children}
     </div>
   );
