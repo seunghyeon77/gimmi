ad44289772755b10d545cdd1589ea2ae8c938e07
seunghyeon77 <jdyjsh77@naver.com>
2024-10-25 14:26:55 +0900

    style: 네브바 상하선 스타일 수정

--- src/app/(afterLogin)/workspace/layout.tsx
+++ src/app/(afterLogin)/workspace/layout.tsx
@@ -11,14 +11,14 @@ export default function Layout({ children }: Props) {
     <div className="px-4 py-12 bg-custom-gradient2 h-full">
       <BackArrow />
       <nav className="my-3">
-        <hr className="border w-screen -mx-6" />
+        <hr className="border w-screen -mx-4" />
         <ul className="flex text-sm gap-x-11 sm:gap-x-8 lg:gap-x-12 justify-center my-2.5">
           <li>그룹홈</li>
           <li>운동하기</li>
           <li>그룹채팅</li>
           <li>운동인증</li>
         </ul>
-        <hr className="border w-screen -mx-6" />
+        <hr className="border w-screen -mx-4" />
       </nav>
       {children}
     </div>
