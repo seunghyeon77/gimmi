c3787c5e6d398c80a0d08f21ab458609ed5cddf7
seunghyeon77 <jdyjsh77@naver.com>
2024-10-27 22:15:30 +0900

    style: 뒤로가기 버튼 크기 변경

--- src/app/(afterLogin)/_components/BackArrow.tsx
+++ src/app/(afterLogin)/_components/BackArrow.tsx
@@ -1,14 +1,14 @@
-'use client';
+"use client";
 
-import backArrow from '@/../public/svgs/backArrow.svg';
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import backArrow from "@/../public/svgs/backArrow.svg";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function BackArrow() {
   const router = useRouter();
   return (
-    <div className="mb-5" onClick={() => router.back()}>
-      <Image src={backArrow} alt="backArrow" />
+    <div onClick={() => router.back()}>
+      <Image className="w-6 h-6" src={backArrow} alt="backArrow" />
     </div>
   );
 }
